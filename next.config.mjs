aa75abf3cb174d1930af8b8d6d55940b07960f5d
Ali_jan <hassanfarid667@gmail.com>
2024-12-12 01:02:56 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,29 @@
   // };
   
   // export default nextConfig;
-  /** @type {import('next').NextConfig} */
-const nextConfig = {};
+//   /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+// const isWindows = process.platform === "win32";
+
+// module.exports = {
+//     swcMinify: true, // Enable SWC minifier
+//     experimental: {
+//         swcLoader: !isWindows, // Use SWC loader only on non-Windows
+//         swcMinify: !isWindows, // Use SWC minifier only on non-Windows
+//     },
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: true, // Enable SWC minifier
+  experimental: {
+      swcLoader: process.platform !== "win32", // Use SWC loader only on non-Windows
+      swcMinify: process.platform !== "win32", // Use SWC minifier only on non-Windows
+  },
+};
 
 export default nextConfig;
-
-
-  
