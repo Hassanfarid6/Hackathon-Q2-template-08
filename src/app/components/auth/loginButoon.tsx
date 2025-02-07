3190509900967d45be7ea7b63936e1737188c11a
Ali_jan <hassanfarid667@gmail.com>
2025-02-07 22:43:56 +0500

    navbar responsive

--- src/app/components/auth/loginButoon.tsx
+++ src/app/components/auth/loginButoon.tsx
@@ -10,7 +10,11 @@ export default function SignInButtonComponent() {
 
   return (
     !isSignedIn && (
-      <button className="flex items-center px-3 py-2 bg-blue-100 border-2 border-slate-700 text-black text-xs rounded hover:bg-yellow-100 mx-2">
+      // flex items-center px-3 py-2 bg-blue-100 border-2 border-slate-700 text-black text-xs rounded hover:bg-yellow-100 mx-2
+      <button className="bg-blue-100 hover:bg-blue-300 font-medium 
+             text-sm sm:text-base md:text-lg xl:text-xl text-black
+             px-4 py-2 sm:px-5 sm:py-2.5 md:px-6 md:py-3 
+             rounded-md transition duration-200">
         <SignInButton>Sign In</SignInButton>
       </button>
     )
