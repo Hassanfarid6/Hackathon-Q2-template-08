16b72175af7e19cc6574f080eeccb34fabfca31b
Ali_jan <hassanfarid667@gmail.com>
2025-05-07 04:40:01 +0500

    updated login button

--- src/app/components/auth/loginButoon.tsx
+++ src/app/components/auth/loginButoon.tsx
@@ -11,10 +11,9 @@ export default function SignInButtonComponent() {
   return (
     !isSignedIn && (
       // flex items-center px-3 py-2 bg-blue-100 border-2 border-slate-700 text-black text-xs rounded hover:bg-yellow-100 mx-2
+      //  text-sm sm:text-base md:text-lg xl:text-xl text-black px-4 py-2 sm:px-5 sm:py-2.5 md:px-6 md:py-3 
       <button className="bg-blue-100 hover:bg-blue-300 font-medium 
-             text-sm sm:text-base md:text-lg xl:text-xl text-black
-             px-4 py-2 sm:px-5 sm:py-2.5 md:px-6 md:py-3 
-             rounded-md transition duration-200">
+                 text-base text-gray-800 px-2 mb-2 ml-2 mr-2 rounded-md transition duration-200">
         <SignInButton>Sign In</SignInButton>
       </button>
     )
